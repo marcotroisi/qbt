55ec697bcedac3c2cae6049b05c734df1fad52f5
Marco Troisi <hello@marcotroisi.com>
2016-07-25 08:07:22 +0100

    Action: tests

--- lib/action_test.go
+++ lib/action_test.go
@@ -6,7 +6,13 @@ import (
 
 func TestAction(t *testing.T) {
 	action := NewActionFromString("test unit")
-	arguments := action.action
+	arguments := action.Arguments()
 
-	// run tests here...
+	if arguments[0] != "test" {
+		t.Error("expected first argument to be test, got ", arguments[0])
+	}
+
+	if arguments[1] != "unit" {
+		t.Error("expected second argument to be unit, got ", arguments[1])
+	}
 }
